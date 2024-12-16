3a0320022c4fe1793676074718260bc8321ce356
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-12-16 12:11:14 -0300

    Adição do tipo doc no admin

--- sao_sebastiao/admin.py
+++ sao_sebastiao/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Documento
+from .models import Documento, Tipo_doc
 # Register your models here.
 
 # Register your models here.
 admin.site.register(Documento)
+admin.site.register(Tipo_doc)
